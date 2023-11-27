44a7390ca1dc3bf0784a27e12abf38cc4d59d19b
GuiBrum22 <123769873+GuiBrum22@users.noreply.github.com>
2023-11-27 16:14:33 -0300

    Update ConnectionFactory.java

--- src/main/java/Connection/ConnectionFactory.java
+++ src/main/java/Connection/ConnectionFactory.java
@@ -1,4 +1,4 @@
-package Connection;
+package persistencia; // Considere um nome de pacote mais específico, como "persistencia" ou "acessobd."
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,29 +7,36 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    
-    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String usuario = "postgres"; 
-    private static final String senha = "postgres"; 
 
+    // Detalhes da conexão com o banco de dados
+    private static final String url = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String usuario = "postgres";
+    private static final String senha = "postgres";
+
+    // Obter uma conexão com o banco de dados
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(url, usuario, senha);
         } catch (SQLException e) {
+            // Tratamento de exceção: Lança uma exceção de tempo de execução com uma mensagem significativa.
             throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
         }
     }
 
+    // Fechar a conexão com o banco de dados
     public static void closeConnection(Connection connection) {
         try {
+            // Usando try-with-resources para gerenciamento automático de recursos
             if (connection != null) {
                 connection.close();
             }
         } catch (SQLException ex) {
+            // Tratamento de exceção: Registre o erro ou manipule de acordo com os requisitos da sua aplicação.
             ex.printStackTrace();
         }
     }
 
+    // Fechar a conexão com o banco de dados e o PreparedStatement
     public static void closeConnection(Connection connection, PreparedStatement stmt) {
         closeConnection(connection);
         try {
@@ -41,6 +48,7 @@ public class ConnectionFactory {
         }
     }
 
+    // Fechar a conexão com o banco de dados, o PreparedStatement e o ResultSet
     public static void closeConnection(Connection connection, PreparedStatement stmt, ResultSet rs) {
         closeConnection(connection, stmt);
         try {
